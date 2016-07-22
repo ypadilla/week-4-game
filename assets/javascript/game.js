9c4c703c464191aec278027be0a99cb124779624
Yavi Padilla <yavipadilla@The-Engineers-MacBook-Air.local>
2016-07-22 17:04:29 -0400

    test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,112 +1,113 @@
 	$(document).ready(function() {
-	var counter = 0;
-	var wins = 0;
-	var losses = 0;
-	$('#win').text(wins);
-	$('#loss').text(losses);
-	
+		var wins = 0;
+		var losses = 0;
+		$("#win").text(wins);
+		$("#loss").text(losses);
+		var counter = 0;
 
-newGame();
+		newGame();
 
-function newGame() {
 
-		counter = 0;
-		$('#scoredTotal').text(counter);
 
-		function randomIntFromInterval(min,max){
-		   	return Math.floor(Math.random()*(max-min+1)+min);
+		function newGame() {
+
+			counter = 0;
+			$('#scoredTotal').text(counter);
+
+			function randomStarterNumber(min,max){
+				return Math.floor(Math.random()*(max-min+1)+min);
 			}
 
-		var numberGuesser = randomIntFromInterval(19,120);
+			var numberGuesser = randomStarterNumber(19,120);
 
-		$('.numberValue').text(numberGuesser);
-		
+			$('.numberValue').text(numberGuesser);
+			
 
-$('#blueG').on('click', function(){
-  	
+			$('#blueG').on('click', function(){
+				
 
-		    counter = counter + Math.ceil(Math.random()*12);
+				counter = counter + Math.ceil(Math.random()*12);
 
-		   	
-		    $('#scoredTotal').text(counter);
+				
+				$('#scoredTotal').text(counter);
 
-		    if (counter == numberGuesser){
-		      $('#status').text('You won!!!!');
-		      wins ++;
-		      $('#win').text(wins);
-		      newGame();
-		        
-		    } else if ( counter > numberGuesser){
-		        $('#status').text('You lost!');
-		        losses ++;
-		        $('#loss').text(losses);
-		        newGame();
-		    	    }
-		});
+				if (counter == numberGuesser){
+					$('#status').text('You win!');
+					wins ++;
+					$('#win').text(wins);
+					newGame();
+					
+				} else if ( counter > numberGuesser){
+					$('#status').text('You lose!');
+					losses ++;
+					$('#loss').text(losses);
+					newGame();
+				}
+			});
 
-$('#yellowG').on('click', function(){
-  	
+			$('#yellowG').on('click', function(){
+				
 
-		    counter = counter + Math.ceil(Math.random()*12);
-		   
-		    $('#scoredTotal').text(counter);
+				counter = counter + Math.ceil(Math.random()*12);
+				
+				$('#scoredTotal').text(counter);
 
-		    if (counter == numberGuesser){
-		      $('#status').text('You won!!!!');
-		      wins ++;
-		      $('#win').text(wins);
-		      newGame();
-		        
-		    } else if ( counter > numberGuesser){
-		        $('#status').text('You lost!');
-		        losses ++;
-		        $('#loss').text(losses);
-		        newGame();
-		    	    }
-		});
+				if (counter == numberGuesser){
+					$('#status').text('You win!');
+					wins ++;
+					$('#win').text(wins);
+					newGame();
+					
+				} else if ( counter > numberGuesser){
+					$('#status').text('You lose!');
+					losses ++;
+					$('#loss').text(losses);
+					newGame();
+				}
+			});
 
 
-$('#greenG').on('click', function(){
-  	
+			$('#greenG').on('click', function(){
+				
 
-		    counter = counter + Math.ceil(Math.random()*12);
-		   
-		    $('#scoredTotal').text(counter);
+				counter = counter + Math.ceil(Math.random()*12);
+				
+				$('#scoredTotal').text(counter);
 
-		    if (counter == numberGuesser){
-		      $('#status').text('You won!!!!');
-		      wins ++;
-		      $('#win').text(wins);
-		      newGame();
-		        
-		    } else if ( counter > numberGuesser){
-		        $('#status').text('You lost!');
-		        losses ++;
-		        $('#loss').text(losses);
-		        newGame();
-		    	    }
-		});
-	
-		$('#redG').on('click', function(){
-  	
+				if (counter == numberGuesser){
+					$('#status').text('You win!');
+					wins ++;
+					$('#win').text(wins);
+					newGame();
+					
+				} else if ( counter > numberGuesser){
+					$('#status').text('You lose!');
+					losses ++;
+					$('#loss').text(losses);
+					newGame();
+				}
+			});
+			
+			$('#redG').on('click', function(){
+				
 
-		    counter = counter + Math.ceil(Math.random()*12);
-		   
-		    $('#scoredTotal').text(counter);
+				counter = counter + Math.ceil(Math.random()*12);
+				
+				$('#scoredTotal').text(counter);
 
-		    if (counter == numberGuesser){
-		      $('#status').text('You won!!!!');
-		      wins ++;
-		      $('#win').text(wins);
-		      newGame();
-		        
-		    } else if ( counter > numberGuesser){
-		        $('#status').text('You lost!');
-		        losses ++;
-		        $('#loss').text(losses);
-		        newGame();
-		    	    }
-		});
-	}
+				if (counter == numberGuesser){
+					$('#status').text('You win!');
+					wins ++;
+					$('#win').text(wins);
+					newGame();
+					
+				} else if ( counter > numberGuesser){
+					$('#status').text('You lose!');
+					losses ++;
+					$('#loss').text(losses);
+					newGame();
+				}
+			});
+		}
 
-});
+	});
